295a3e32a74ca4286fbfb57a069b11381626d8a4
amadi5892 <amadi5892@gmail.com>
2022-12-07 23:10:43 -0500

    IncomeExpense component is functional

--- src/components/IncomeExpense/IncomeExpense.component.jsx
+++ src/components/IncomeExpense/IncomeExpense.component.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/global.context';
+
 import './IncomeExpense.styles.css';
 
 const IncomeExpense = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+    const expense = amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item),0)
+    .toFixed(2);
+
   return (
     <div className='incomeExpense-container' >
         <div>
             <h4>
                 Income
             </h4>
-            <span className='money plus' >+$0.00</span>
+            <span className='money plus' >{income}</span>
         </div>
         <div className='divider' ></div>
         <div >
             <h4>
                 Expense
             </h4>
-            <span className='money minus' >-$0.00</span>
+            <span className='money minus' >{Math.abs(expense)}</span>
         </div>
     </div>
   )
